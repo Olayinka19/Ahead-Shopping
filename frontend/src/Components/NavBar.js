5b5b0a9cb9c3ab3df3cba5fa4d9388b9504f1c7b
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2023-08-12 02:10:38 -0400

    product update

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -33,7 +33,7 @@ function NavBar() {
                 {/* <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #offcanvas-nav-primary">Menu</button> */}
 
 <button class="uk-button uk-button-default" type="button" uk-toggle="target: #offcanvas-nav"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iconbolt.com%2Fpreview%2Ffacebook%2Fdripicons%2Fmenu.svg&f=1&nofb=1&ipt=3dad0d32135d4c435bb7097284feac2dae6061687936bc992985f600f871a4b6&ipo=images" width="30px" height="30px" />Menu</button>
-<button class="uk-button uk-button-default" type="button" uk-toggle="target: #offcanvas-nav"> <Link to="/Items">🛍️ Shop</Link></button>
+<button class="uk-button uk-button-default"> <Link to="/Items">🛍️ Shop</Link></button>
 
 
 <CartIcon initialItemCount={initialCartItemCount} />
